44f2848aad23c457319dbb81226d64e6a4b04fb6
hasnatsajid965 <86589885+hasnatsajid@users.noreply.github.com>
2022-07-20 17:24:48 +0500

    added morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
 
 const categoryRouter = require('./routes/categories/category.router');
 
@@ -11,7 +13,11 @@ app.use(
   })
 );
 
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(categoryRouter);
 
 module.exports = app;
